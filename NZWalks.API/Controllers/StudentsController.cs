8215c1838117f41cb8b590494515c97b355a906f
Bil Bk <bilbk311@gmail.com>
2026-05-10 14:29:34 -0500

    Commit on 5/10/2026

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:44328/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //https://localhost:44328/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new[]
+            {
+                "John Doe",
+                "Jane Smith",
+                "Michael Johnson"
+            };
+            return Ok(students);
+        }
+    }
+}
